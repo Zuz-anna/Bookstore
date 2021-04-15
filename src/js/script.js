33b2181890056a04095995a08e216d225d185a09
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-04-15 16:23:00 +0200

    Add javascript code - constructor methods - getElements/renderBooks/initActions

--- src/js/script.js
+++ src/js/script.js
@@ -35,13 +35,36 @@
 
       thisBooks.id = id;
       thisBooks.data = data;
-
       thisBooks.getElements();
       thisBooks.renderBooks();
       thisBooks.initActions();
 
     };
 
+    getElements() {
+      const thisBooks = this;
 
+      thisBooks.bookList = document.querySelector(select.containerOf.bookList);
+    };
+
+    renderBooks() {
+      const thisBooks = this;
+
+      for ( const data of dataSource.books ) {
+        const generateHTML = templates.bookTemplate(data);
+        const generatedDOM = utils.createDOMFromHTML(generateHTML);
+
+        thisBooks.bookList.appendChild(generatedDOM);
+      };
+    };
+
+    initActions() {
+      const thisBooks = this;
+    };
+  }
+
+  const app = new Books();
+
+  app.init();
 };
 
